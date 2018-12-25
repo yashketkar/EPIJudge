27ec5a0e8e884dee24042d7dc1799f6278bcfd5f
Yash Ketkar <yash.ketkar@cerner.com>
2018-12-25 00:04:44 -0600

    5.2

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -1,9 +1,16 @@
 from test_framework import generic_test
 
-
+# https://leetcode.com/problems/plus-one/submissions/
 def plus_one(A):
-    # TODO - you fill in here.
-    return []
+    A[-1]+=1
+    for i in range(len(A)-1, 0, -1):
+        if A[i] >= 10:
+            A[i]-=10
+            A[i-1]+=1
+    if A[0]>= 10:
+        A[0]-=10
+        A = [1] + A
+    return A
 
 
 if __name__ == '__main__':
