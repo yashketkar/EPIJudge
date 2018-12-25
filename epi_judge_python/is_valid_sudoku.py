cef5a765afd6571f2101f29e71706f2a71e22936
Yash Ketkar <yash.ketkar@cerner.com>
2018-12-25 03:09:53 -0600

    5.17

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
 
+def contains_duplicates(pa):
+    no_zero = [y for y in pa if y != 0]
+    if len(set(no_zero)) != len(no_zero):
+        return True
+    return False
 
+# 36. https://leetcode.com/problems/valid-sudoku/
+# 5.17
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment):
-    # TODO - you fill in here.
+    pa = partial_assignment
+    for row in pa:
+        if contains_duplicates(row):
+            return False
+    for i in range(len(pa[0])):
+        col = [x[i] for x in pa]
+        if contains_duplicates(col):
+            return False
+    for i in [0,3,6]:
+        for j in [0,3,6]:
+            sub = [pa[i+0][j+0], pa[i+0][j+1], pa[i+0][j+2], pa[i+1][j+0], pa[i+1][j+1], pa[i+1][j+2], pa[i+2][j+0], pa[i+2][j+1], pa[i+2][j+2]]
+            if contains_duplicates(sub):
+                return False    
     return True
 
 
