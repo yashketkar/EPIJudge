7add57285d0455756c43dd59ecae418a0e698548
Yash Ketkar <yash.ketkar@cerner.com>
2018-12-30 00:45:09 -0600

    8.1, 8.2

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,32 @@
 from test_framework import generic_test
 
 
+def calculate(a, b, o):
+    if o == '+':
+        return a + b
+    elif o == '-':
+        return a - b
+    elif o == '*':
+        return a * b
+    elif o == '/':
+        return a // b
+    else:
+        print('error occured: {0} is not an operation'.format(o))
+        return 0
+
+
 def evaluate(expression):
-    # TODO - you fill in here.
-    return 0
+    if not expression:
+        return 0
+    intermediate_results = []
+    for x in expression.split(','):
+        if x in '+-*/':
+            b = intermediate_results.pop()
+            a = intermediate_results.pop()
+            intermediate_results.append(calculate(a, b, x))
+        else:
+            intermediate_results.append(int(x))
+    return intermediate_results[-1]
 
 
 if __name__ == '__main__':
--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -3,21 +3,28 @@ from test_framework.test_failure import TestFailure
 
 
 class Stack:
+    def __init__(self):
+        self.current_max = None
+        self.stack = []
+        return
+
     def empty(self):
-        # TODO - you fill in here.
-        return True
+        return False if self.stack else True
 
     def max(self):
-        # TODO - you fill in here.
-        return 0
+        return self.current_max if self.current_max else 0
 
     def pop(self):
-        # TODO - you fill in here.
-        return 0
+        if not self.stack:
+            return None
+        x = self.stack.pop()
+        self.current_max = self.stack[-1][-1] if self.stack else None
+        return x[0]
 
     def push(self, x):
-        # TODO - you fill in here.
-        return
+        self.current_max = max(self.current_max, x) if self.current_max else x
+        self.stack.append((x, self.current_max))
+        return None
 
 
 def stack_tester(ops):
