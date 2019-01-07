6c0993100f2dff8dfe0aaed7815e1f62f308a210
Yash Ketkar <yashketkar@hotmail.com>
2019-01-06 22:37:46 -0600

    9.11

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
+from binary_tree_node import BinaryTreeNode
+
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+# 9.11
+def helper(preorder, inorder, dic, pi, pj, ii, ij):
+    if pj <= pi or ij <= ii:
+        return None
+    i = dic[preorder[pi]]
+    left_size = i - ii
+    root = BinaryTreeNode(preorder[pi])
+    root.left = helper(preorder, inorder, dic, pi+1, pi+1+left_size, ii, i)
+    root.right = helper(preorder, inorder, dic, pi+1+left_size, pj, i+1, ij)
+    return root
 
 
 def binary_tree_from_preorder_inorder(preorder, inorder):
-    # TODO - you fill in here.
-    return None
+    dic = {data:i for i, data in enumerate(inorder)}
+    root = helper(preorder, inorder, dic, 0, len(preorder), 0, len(inorder))
+    return root
 
 
 if __name__ == '__main__':
