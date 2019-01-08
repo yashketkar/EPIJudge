9bf47879f8d8f420f706624d757969992af6b4c4
Yash Ketkar <yash.ketkar@cerner.com>
2019-01-08 00:33:20 -0600

    11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    l, r, result = 0 , len(A)-1, -1
+    while l<=r:
+        mid = (l+r)//2
+        if A[mid] > k:
+            r = mid - 1
+        elif A[mid] == k:
+            result = mid
+            r = mid - 1
+        else:
+            l = mid + 1
+    return result
 
 
 if __name__ == '__main__':
