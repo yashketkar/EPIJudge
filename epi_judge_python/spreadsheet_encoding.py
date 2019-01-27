83a25ba980bdbb5c4fa913a55720a54e4c17dd0b
Yash Ketkar <yash.ketkar@cerner.com>
2019-01-27 14:01:11 -0600

    6.3

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -1,9 +1,13 @@
 from test_framework import generic_test
+import functools
 
 
 def ss_decode_col_id(col):
-    # TODO - you fill in here.
-    return 0
+    result = 0
+    # for i in col:
+    #     result = result*26 + (ord(i)-ord('A')+1)
+    result = functools.reduce( lambda r, c: r*26 + (ord(c)-ord('A')+1), col, 0)
+    return result
 
 
 if __name__ == '__main__':
