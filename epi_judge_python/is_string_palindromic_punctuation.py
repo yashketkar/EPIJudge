a91440aa595885e4759bbf29b8c280cfb2a047e5
Yash Ketkar <yash.ketkar@cerner.com>
2019-01-27 15:38:25 -0600

    6.5 and 6.8

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -1,8 +1,21 @@
 from test_framework import generic_test
+import string
 
 
 def is_palindrome(s):
-    # TODO - you fill in here.
+    i,j = 0, len(s)-1
+    punctuation = set(string.punctuation)
+    punctuation.add(' ')
+    while i<j:
+        while s[i] in punctuation:
+            i+=1
+        while s[j] in punctuation:
+            j-=1
+        if s[i].lower() == s[j].lower():
+            i+=1
+            j-=1
+        else:
+            return False
     return True
 
 
--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -1,9 +1,19 @@
 from test_framework import generic_test
 
 
+# 6.8 Roman to Integer
 def roman_to_integer(s):
-    # TODO - you fill in here.
-    return 0
+    mapping = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500,
+               'M':1000}
+    total, i = 0, 0
+    while i < len(s):
+        if (i+1)<len(s) and mapping[s[i]] < mapping[s[i+1]]:
+            total += mapping[s[i+1]]-mapping[s[i]]
+            i += 1
+        else:
+            total += mapping[s[i]]
+        i += 1
+    return total
 
 
 if __name__ == '__main__':
